eefbee479e74e2e29e381955c8270a31c1a8ddfc
Lanternals <166974252+Lanternals@users.noreply.github.com>
2024-04-16 21:24:42 -0500

    Update config.js

--- styles/config.js
+++ styles/config.js
@@ -2,7 +2,7 @@
 /* Sheet ID
 /* Your sheet ID
 /* ------------------------------------------------------------------- */
-let sheetID = "1GwgfLizD3HQCieGia6di-TfU4E3EipT9Jb0BDZQwNak";
+let sheetID = "1bKw3I2_C7YZtmzDHXjZrciYou3N0RE7IVZJ2MS9F5_E";
 
 
 /* ------------------------------------------------------------------- */
